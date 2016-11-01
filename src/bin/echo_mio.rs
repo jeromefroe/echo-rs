5a906a440f3edc24b95f0f257b2f66377c8a6dcb
Jerome Froelich <jeromefroe@uber.com>
2016-11-01 18:22:12 -0400

    [echo_mio] Add timer to list number of open connections and deregister closed connections from Poll

--- src/bin/echo_mio.rs
+++ src/bin/echo_mio.rs
@@ -1,11 +1,29 @@
 extern crate mio;
 
 use std::io::{Read, Write};
+use std::time::Duration;
 use std::collections::HashMap;
-use mio::{Token, Ready, Events, Poll, PollOpt};
-use mio::tcp::TcpListener;
+use mio::{Token, Ready, Events, Poll, PollOpt, Evented};
+use mio::tcp::{TcpListener, TcpStream};
+use mio::timer::Timer;
+
+struct Connection {
+    buf: [u8; 512],
+    handle: TcpStream,
+}
+
+impl Connection {
+    fn new(handle: TcpStream) -> Self {
+        Connection {
+            buf: [0; 512],
+            handle: handle,
+        }
+    }
+}
 
 const SERVER: Token = Token(0);
+const TIMER_TOKEN: Token = Token(1);
+
 const ADDR: &'static str = "127.0.0.1:8888";
 
 pub fn main() {
@@ -16,13 +34,19 @@ pub fn main() {
     // create a buffer to read into
     let mut buf = [0; 512];
 
-    let mut count = 0;
+    let mut count = 1;
     let mut connections = HashMap::new();
 
     poll.register(&server, SERVER, Ready::readable(), PollOpt::edge()).unwrap();
 
     // Create storage for events
     let mut events = Events::with_capacity(1024);
+
+    let mut timer = Timer::default();
+    poll.register(&timer, TIMER_TOKEN, Ready::readable(), PollOpt::edge())
+        .unwrap();
+    let flush_timeout = Duration::from_millis(5000);
+    timer.set_timeout(flush_timeout, String::from("hello from 1s ago")).ok();
 
     loop {
         poll.poll(&mut events, None).unwrap();
@@ -41,8 +65,14 @@ pub fn main() {
                     poll.register(&stream, Token(count), Ready::readable(), PollOpt::edge())
                         .unwrap();
 
-                    connections.insert(count, Box::new(stream));
+                    let conn = Connection::new(stream);
 
+                    connections.insert(count, conn);
+
+                }
+                TIMER_TOKEN => {
+                    println!("number of open connections: {}", connections.len());
+                    timer.set_timeout(flush_timeout, String::from("hello from 1s ago")).ok();
                 }
                 Token(c) => {
                     let n;
@@ -50,9 +80,9 @@ pub fn main() {
                     // need to get stream in a seperate scope in case client closes the connection
                     // in which case we want to drop it
                     {
-                        let stream = connections.get_mut(&c).unwrap();
+                        let conn = connections.get_mut(&c).unwrap();
 
-                        n = match stream.read(&mut buf) {
+                        n = match conn.handle.read(&mut buf) {
                             Ok(m) => m,
                             Err(e) => panic!("got an error when reading from connection: {}", e),
                         };
@@ -60,7 +90,7 @@ pub fn main() {
                         if n != 0 {
                             println!("read {} bytes from client", n);
 
-                            match stream.write(&buf[..n]) {
+                            match conn.handle.write(&buf[..n]) {
                                 Ok(n) => println!("wrote {} bytes from client", n),
                                 Err(e) => panic!("got an error when writing to connection: {}", e),
                             }
@@ -70,7 +100,8 @@ pub fn main() {
                     if n == 0 {
                         // EOF, client closed connection so we can drop it now
                         println!("client closed connection");
-                        connections.remove(&c);
+                        let conn = connections.remove(&c).unwrap();
+                        poll.deregister(&conn.handle);
                     }
                 }
             }
